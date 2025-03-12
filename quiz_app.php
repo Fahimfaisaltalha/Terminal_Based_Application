5e47af6d4ed71734a78fd7deec19c29ce519a63d
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-03-12 19:49:49 +0600

    Finished the project

--- quiz_app.php
+++ quiz_app.php
@@ -1,26 +1,42 @@
 <?php
 
-function evaluateQuiz(array $questions, array $answers): int{
-    $score =0;
+function evaluateQuiz(array $questions, array $answers): int
+{
+    $score = 0;
 
-    for($i=0;$i<count($questions);$i++){
-        if(strtolower($answers[$i])=== strtolower($questions[$i]['correct'])){
+    for ($i = 0; $i < count($questions); $i++) {
+        if (strtolower($answers[$i]) === strtolower($questions[$i]['correct'])) {
             $score++;
         }
-
     }
 
     return $score;
-
-
-
 }
 
-$questions=[['question'=> 'What is 73 + 89 ?','correct'=>'162'],
-            ['question'=> 'What is the capital of Bangladesh ?','correct'=>'Dhaka'],
-            ['question'=> 'How many districts in Bangladesh ?','correct'=>'64'],
-            ['question'=> 'How many division in Bangladesh ?','correct'=>'8'],
+$questions = [
+    ['question' => 'What is 73 + 89 ?', 'correct' => '162'],
+    ['question' => 'What is the capital of Bangladesh ?', 'correct' => 'Dhaka'],
+    ['question' => 'How many districts in Bangladesh ?', 'correct' => '64'],
+    ['question' => 'How many division in Bangladesh ?', 'correct' => '8'],
 ];
 
+$answers = [];
+echo "Welcome to the Terminal base Quiz Application \n";
+echo "Please Enter the following Question : \n";
 
+foreach ($questions as $index => $question) {
+    echo ($index + 1) . "." . $question['question'] . "\n";
+    echo "Your Answer :";
+    $answers[] = readline();
+}
+$totalQuestions = count($questions);
+$score = evaluateQuiz($questions, $answers);
 
+echo "You scored $score out of $totalQuestions.\n";
+if ($score === $totalQuestions) {
+    echo "Excellent job!\n";
+} elseif ($score > $totalQuestions / 2) {
+    echo "Good effort!\n";
+} else {
+    echo "Better luck next time!";
+}
